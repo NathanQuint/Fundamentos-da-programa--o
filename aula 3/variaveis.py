b224749bbc797056e555e3ec67bbf0e85d4f13b8
Nathan <nathanqc0109@gmail.com>
2026-03-17 09:31:51 -0300

    Subindo os primeiros arquivos

---
+++ aula 3/variaveis.py
@@ -0,0 +1,14 @@
+# Variaveis e Tipos de Dados
+# Em Python, você não precisa declarar o tipo de uma variavel antes de usá-la. O tipo é inferido automaticamente pelo interpretador.
+# EM Python, a variavel é declarada no padrão snake_case
+# 1. string ->
+# 2. number - int (numero inteiro) ->
+# 3. number - float (numero decimal) ->
+# 4. boolen - True or False (verdadeiro ou falso) ->
+
+nome = "Nathan" # string
+email = "nathanqc0109@gmail.com" # string
+cidade = "Rio de Janeiro" # string
+cpf = 186448853 # number-int
+salario = 14777.76 # number-float
+casado = False # boolen
---
+++ aula 4/operadores.py
@@ -0,0 +1,35 @@
+# Operadores Matemáticos  
+# 1. + Soma
+# 2. - subtração
+# 3. * multiplicação
+# 4. / divisão
+# 5. ** potência
+# 6. % módulo
+
+numero_um = 20
+numero_dois = 40
+soma = numero_um + numero_dois
+print(f"A soma entre {numero_um} + {numero_dois} é {soma}")
+
+numero_tres = 1
+numero_quatro = 2
+
+subtracao = numero_tres - numero_quatro
+
+print (f"A subtração entre {numero_tres} - {numero_quatro} é {subtracao}")
+
+numero_cinco = 100
+numero_seis = 5
+
+multiplicacao = numero_cinco * numero_seis
+print(f"A multiplicação entre {numero_cinco} * {numero_seis} é {multiplicacao}")
+
+numero_sete = 50
+numero_oito = 10
+
+divisao = numero_sete / numero_oito
+print(f"A divisão entre {numero_sete} / {numero_oito} é {divisao}")
+
+
+
+
